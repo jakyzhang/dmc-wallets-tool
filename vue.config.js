188b0dce48936735d31441bb513054a6f379260f
jakyzhang@live.com <jakyzhang@live.com>
2023-02-11 12:50:49 +0800

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 //set publicPath for deploy to github pages
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
-        ? '/dmc-wallet-tool/'
+        ? '/dmc-wallets-tool/'
         : '/'
 }
